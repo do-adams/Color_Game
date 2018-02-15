42008f00411173f18e3a4a7e8c407d6064a4139d
Damián Adams <contact@mianlabs.com>
2018-02-15 19:04:43 -0400

    Add the New Colors button functionality for resetting the game
    The JS code is starting to look messy, this needs to be refactored pretty soon 🤧

--- colorGame.js
+++ colorGame.js
@@ -5,6 +5,17 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(let i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323"; // original bg color
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -15,10 +26,11 @@ for(let i = 0; i < squares.length; i++) {
 		let clickedColor = this.style.backgroundColor;
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else {
-			// set to the page background color
+			// set to the original background color
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
 		}
