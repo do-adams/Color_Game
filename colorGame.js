1f1ad1ba3fb40178ee748742837b09367bf3ad1b
Damián Adams <contact@mianlabs.com>
2018-02-16 16:36:53 -0400

    Set the text content of the reset button to its initial value when clicked
    Fixes a problem when users click on the reset button after winning the game for the purpose of playing a new game and the reset button text still reads as "Play Again?"

--- colorGame.js
+++ colorGame.js
@@ -45,6 +45,7 @@ resetButton.addEventListener("click", function() {
 	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
+	this.textContent = "New Colors";
 	messageDisplay.textContent = "";	
 	for(let i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
